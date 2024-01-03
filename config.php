897a3c629bbfb67ca390123b79133144aaeca19e
CetaceaWang <110277682+CetaceaWang@users.noreply.github.com>
2024-01-03 09:19:06 +0800

    1.03
    因應中央氣象局於112年9月15日改制為中央氣象署，資料下載URL與各項API服務改為https://opendata.cwa.gov.tw/

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //$Sunset_URL="https://opendata.cwb.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=rdec-key-123-45678-011121314&locationName=%E8%87%BA%E4%B8%AD%E5%B8%82&timeFrom=2022-10-29&timeTo=2022-11-29&sort=dataTime"; 
-$Sunset_URL="https://opendata.cwb.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=rdec-key-123-45678-011121314&CountyName=%E8%87%BA%E4%B8%AD%E5%B8%82&timeFrom=2022-10-29&timeTo=2022-11-29&sort=dataTime"; 
-$Tidal_URL="https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-A0021-001?Authorization=rdec-key-123-45678-011121314&LocationName=%E8%87%BA%E4%B8%AD%E5%B8%82%E6%B8%85%E6%B0%B4%E5%8D%80&sort=Date,DateTime";
+$Sunset_URL="https://opendata.cwa.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=rdec-key-123-45678-011121314&CountyName=%E8%87%BA%E4%B8%AD%E5%B8%82&timeFrom=2022-10-29&timeTo=2022-11-29&sort=dataTime"; 
+$Tidal_URL="https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-A0021-001?Authorization=rdec-key-123-45678-011121314&LocationName=%E8%87%BA%E4%B8%AD%E5%B8%82%E6%B8%85%E6%B0%B4%E5%8D%80&sort=Date,DateTime";
 $WoodenWalkwayFile="WoodenWalkway.txt";       
 ?>
